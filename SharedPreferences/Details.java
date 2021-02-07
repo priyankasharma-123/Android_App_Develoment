ee68f93fc0dee20a6e91fbff8d28bbe1552d40f6
priyankasharma-123 <47389735+priyankasharma-123@users.noreply.github.com>
2021-02-07 14:10:10 +0530

    Create Details.java

---
+++ SharedPreferences/Details.java
@@ -0,0 +1,35 @@
+package com.example.sharedpreferences;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class Details extends AppCompatActivity {
+
+    SharedPreferences prf;
+    Intent intent;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_details);
+        TextView result = (TextView)findViewById(R.id.textView);
+        Button btnLogOut = (Button)findViewById(R.id.LogOut);
+        prf = getSharedPreferences("userdetails",MODE_PRIVATE);
+        intent = new Intent(Details.this,MainActivity.class);
+        result.setText("Hello, "+prf.getString("username",null));
+        btnLogOut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SharedPreferences.Editor editor = prf.edit();
+                editor.clear();
+                editor.commit();
+                startActivity(intent);
+            }
+        });
+    }
+
+}
